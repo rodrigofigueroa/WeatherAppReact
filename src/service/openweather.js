f5c23a8663165619ca3be9f8ee4a737cfdd20905
rodrigofigueroa <olivertate274@gmail.com>
2020-02-25 18:30:10 -0600

    mx

--- src/service/openweather.js
+++ src/service/openweather.js
@@ -14,6 +14,7 @@ export const GET_API_WEATHER_IMG = (icon) => {
 
 export const GET_API_WEATHER_EXACT_PATH = (zip,countryCode) =>{
     const key            = '56ef8cfe0a0091a259799e537a7aaac9';
+    const mx = 'mx'
     const BASE_URL_EXACT = `http://openweathermap.org/data/2.5/weather?zip=${55080},${mx}&appid=${key}`
     return(`${BASE_URL_EXACT}`)
 }
