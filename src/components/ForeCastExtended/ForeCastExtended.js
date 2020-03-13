44a7434c71c166192ced5c496f3f9380cfc7a10e
rodrigofigueroa <olivertate274@gmail.com>
2020-03-13 13:52:26 -0600

    new things for forecast

--- src/components/ForeCastExtended/ForeCastExtended.js
+++ src/components/ForeCastExtended/ForeCastExtended.js
@@ -1,6 +1,6 @@
 import  React,{useEffect, useState} from 'react';
 import  {GET_API_WEATHER_EXACT_PATH} from '../../service/openweather';
-
+import './ForeCastExtended.sass'
 const ForeCastExtended = (props) => {    
     const { cityForeCast } = props;
     const [setKey, setCountryFore] = useState();
@@ -11,7 +11,7 @@ const ForeCastExtended = (props) => {
         switch(city){
             case 'Mexico City, mx':
                 console.log(`city: ${city}`)
-                return '3827406'
+                return [['3827406','Benito Juarez']]
                 break;
             case 'London, uk':
                 console.log(`city: ${city}`)
@@ -21,20 +21,50 @@ const ForeCastExtended = (props) => {
                 break;
             case 'Helsinky, FI':
                 console.log(`city: ${city}`)
+                default :
+                return [['Click in the country of you like',['nothing']]]
         }
     }
-    const ZIP = CityMaper(cityForeCast);
-    const OpenWeather = GET_API_WEATHER_EXACT_PATH(ZIP);
+    const ZIP = CityMaper(cityForeCast);    
+    const OpenWeather = GET_API_WEATHER_EXACT_PATH(ZIP[0][0]);
     
     const ForeNumberDay = (OpenWeather) => {
                    
         if(typeof OpenWeather !== 'undefined' ){
                 if(OpenWeather.cod !== '400'){
+                    console.log(OpenWeather);
+                    // debugger
                     return (
                         OpenWeather.list.map((item,ind) => {
+                            const { 
+                                dt,
+                                main: {
+                                    temp,
+                                    feels_like,
+                                    temp_min,
+                                    temp_max,
+                                    pressure,
+                                    sea_level,
+                                    grnd_level,
+                                    humidity,
+                                    temp_kf,
+                            },
+                            weather : { 
+                                0 : {
+                                    id,
+                                    main,
+                                    description,
+                                    icon,
+                                } 
+                            },
+                            dt_txt
+                        } = item;
                             return (
-                                <div key={ind}>
-                                    <h2>{new Date(item.dt * 1000).toString()}</h2>
+                                <div key={ ind } className="week-forecast">
+                                    <h2>{ new Date(dt * 1000).toString() }</h2>
+                                    <div className="card-week-forecast">
+
+                                    </div>
                                 </div>
                             )
                         })
@@ -55,7 +85,7 @@ const ForeCastExtended = (props) => {
     return(        
         <article className="forecast-extended">
             <h2>ForeCast</h2>
-             <div>{cityForeCast}</div>
+            <div>{ZIP[0][1]} {cityForeCast}</div>
              {ForeNumberDay(setKey)}
         </article>
     )
