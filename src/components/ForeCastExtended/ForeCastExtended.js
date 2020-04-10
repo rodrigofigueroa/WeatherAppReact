6eb6c09d80320adca730860bf13808429a1f640e
rodrigofigueroa <olivertate274@gmail.com>
2020-04-09 20:03:26 -0500

    forecastextended fix it

--- src/components/ForeCastExtended/ForeCastExtended.js
+++ src/components/ForeCastExtended/ForeCastExtended.js
@@ -2,28 +2,47 @@ import  React,{useEffect, useState} from 'react';
 import './ForeCastExtended.sass';
 import {CityMaper} from '../../service/CityMaper';
 import {Cities} from '../../service/Cities';
-
+import WeatherIcon from '../WeatherApp/WeatherLocation/WeatherIcon';
 const ForeCastExtended = (props) => {    
     const { cityForeCast } = props;
     const [setKey, setCountryFore] = useState();
-
+    const [states,setState] = useState();    
     useEffect(() => {            
         const ZIP = CityMaper(cityForeCast); 
         // debugger
-        if(ZIP[0].cityName !== 'nothing'){
+        if(ZIP[0].idCity !== 'Click in the country of you like'){
             Cities(ZIP).then( data =>{ 
                 console.log(data); return setCountryFore( data )});
-        }
+               const NewZip = ZIP.map(ine => ine.cityName)
+                    setState(NewZip)
+                }
     }, [ cityForeCast ])
-
     return(        
         <article className="forecast-extended">
             <h2>ForeCast</h2>
+            {}
              { 
                 setKey ?
-                    setKey.map((itemOne) => {
-                        return itemOne.map((itemHtml) => {
-                            return itemHtml
+                    setKey.map((itemOne,index) => {
+                            let StateNow = states[index]                            
+                        return itemOne.map((itemHtml,ind) => {
+                            const { dt, main: { temp, humidity }, weather : { 0 : { description, icon,} } } = itemHtml;
+                            return (
+                                <div key={ind}>
+                                    <WeatherIcon icon={icon} title={`${description}`}/>
+                                    <h2>{`${StateNow !== undefined ? StateNow : 'ji'}`}</h2>
+                                    <h3>
+                                        {`${new Date(dt * 1000).toString()} `}
+                                    </h3>
+                                    <h4>
+                                        {`${Math.round(temp)} °C`}
+                                    </h4>
+                                    <h5>
+                                        {`humidity ${humidity}%`}
+                                    </h5>
+                                    <p>{`${description}`}</p>
+                                </div>
+                            ) 
                         })
                     })
                 : <h3>Waiting...</h3> }
--- src/service/Cities.js
+++ src/service/Cities.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import  {RETURN_AXIOS_POST} from './openweather';
 import axios from 'axios';
-import WeatherIcon from '../components/WeatherApp/WeatherLocation/WeatherIcon';
 
 export const Cities = async (object) =>{
-    if( object[0].cityName !== 'nothing' ){
-
+    if( object[0].cityName !== 'nothing' ){        
         let newPromise =  await Promise.all(object.map((city) =>{ 
 
             const  OpenWeather =  RETURN_AXIOS_POST(city.idCity);
@@ -15,85 +13,16 @@ export const Cities = async (object) =>{
         })).then(cities => cities);
 
         const MapCititesPromise =  ( PromiseValue ) => {
-                const prom = PromiseValue          
-                // console.log(prom);      
-                // debugger
-             return  prom.map((promis) => {  
-                // debugger
-                return promis.list.filter((item, index) => {
-                    
-                    if(
-                        item.dt_txt === `2020-03-27 03:00:00` ||
-                        item.dt_txt === `${item.dt_txt.substring(0, 10)}  18:00:00` ||
-                        item.dt_txt === `${item.dt_txt.substring(0, 10)}  21:00:00` ){
-
-                            return item.map((index) => {
-                                    const { 
-                                        dt,
-                                        main: {
-                                            temp,
-                                            humidity
-                                    },
-                                    weather : { 
-                                        0 : {                                
-                                            description,
-                                            icon,
-                                        } 
-                                    }
-                                    } = item;
-                                    return (
-                                        <div key={index}>
-                                            <WeatherIcon icon={icon} title={`${promis.city.name} ${description}`}/>
-                                            <h2>{`${promis.city.name}`}</h2>
-                                            <h3>
-                                                {`${new Date(dt * 1000).toString()} `}
-                                            </h3>
-                                            <h4>
-                                                {`${Math.round(temp)} °C`}
-                                            </h4>
-                                            <h5>
-                                                {`humidity ${humidity}%`}
-                                            </h5>
-                                            <p>{`${description}`}</p>
-                                        </div>
-                                    )                                
-                            })
-                        }  
+                const prom = PromiseValue                              
+               return prom.map((promis) => {     
+                 return promis.list.filter((item, index) => {                     
+                     return item.dt_txt !== `${item.dt_txt.substring(0, 10)} 00:00:00` &&
+                     item.dt_txt !== `${item.dt_txt.substring(0, 10)} 03:00:00` &&
+                     item.dt_txt !== `${item.dt_txt.substring(0, 10)} 06:00:00` &&
+                     item.dt_txt !== `${item.dt_txt.substring(0, 10)} 12:00:00`
                 });
-            //    return promis.list.slice(0,10).map((item,index) => {
-            //     debugger
-            //     const { 
-            //             dt,
-            //             main: {
-            //                 temp,
-            //                 humidity
-            //         },
-            //         weather : { 
-            //             0 : {                                
-            //                 description,
-            //                 icon,
-            //             } 
-            //         }
-            //         } = item;
-            //         return (
-            //             <div key={index}>
-            //                 <WeatherIcon icon={icon} title={`${promis.city.name} ${description}`}/>
-            //                 <h2>{`${promis.city.name}`}</h2>
-            //                 <h3>
-            //                     {`${new Date(dt * 1000).toString()} `}
-            //                 </h3>
-            //                 <h4>
-            //                     {`${Math.round(temp)} °C`}
-            //                 </h4>
-            //                 <h5>
-            //                     {`humidity ${humidity}%`}
-            //                 </h5>
-            //                 <p>{`${description}`}</p>
-            //             </div>
-            //         )
-            //     });
-
             });
+            // return dataComplete;
         }
         return MapCititesPromise(newPromise)
     }
