fec6ce6a7ffd34e674d5833b70ae3ccd589ba780
rodrigofigueroa <olivertate274@gmail.com>
2020-05-08 19:56:56 -0500

    managing actions in a dir

--- src/components/WeatherApp/index.js
+++ src/components/WeatherApp/index.js
@@ -2,13 +2,12 @@ import './WeatherApp.sass';
 import React            from 'react';
 import WeatherLocation  from './WeatherLocation';
 import ForeCastExtended from '../ForeCastExtended/ForeCastExtended';
-import {createStore} from 'redux';
+import { createStore } from 'redux';
+import { setCity } from '../../actions';
 //APi
 const countries = ['London, uk','Mexico City, mx','Canada, CA','Helsinki, FI'];
 //redux store 
 const store = createStore(() => {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-const setCity = value => ({ type: 'setCity', value });
 
 class WeatherApp extends React.Component {
     constructor(){
